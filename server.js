7e1483ad82f721bede305553200011e6fa55e4ea
Seamus Brown <shay_brown_13@hotmail.com>
2021-01-27 16:12:37 -0800

    stuff

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 const pg = require('pg');
-const { get } = require('superagent');
 const DATABASE_URL = process.env.DATABASE_URL;
 const client = new pg.Client(DATABASE_URL);
 client.on('error', (error) => {
@@ -22,7 +21,15 @@ app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 // displays our home page
+//these are our routes
 app.get('/', getIndex);
+// app.get('/books', getSearchForm);
+// app.get('/books/:id', getDetails);
+app.get('/booksearch', getBookSearch);
+// app.post('/booksearch', sendBooks);
+
+
+
 
 function getIndex(req, res) {
   const sqlCheck = 'SELECT * FROM books';
@@ -33,39 +40,43 @@ function getIndex(req, res) {
         res.send(savedBooks.rows[0]);
       } else {
         // const sqlCheck2 = 'INSERT INTO books';
-        res.render('/pages/index.ejs');
+        res.render('pages/index.ejs');
       }
     });
   // res.render('pages/index.ejs');
 }
 
 
-app.get('/booksearch', (req, res) => {
-  res.render('pages/searches/new');
-});
-// sending info to googles api
-app.post('/booksearch', search);
-// post use searchTitles req and res
-function search(req, res) {
+
+function getBookSearch(req, res) {
+
+  // sending info to googles api
+
+  // post use searchTitles req and res
   const search = req.body.search;
+  console.log(req.body);
   // checking whether title or author is checked in new.ejs
   if (search[0] === 'title') {
 
     const url = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${search[1]}`;
     superagent.get(url).then(obj => {
       const books = obj.body.items.map(item => new Book(item));
-
-      // console.log(books);
-      res.render('pages/searches/show.ejs', { books: books });
+      const sqlSend = 'INSERT INTO books (title, author, url';
+      books.forEach(obj => {
+        const sqlArr = [books[obj].title, books[obj].author, books[obj].url];
+        client.query(sqlSend, sqlArr);
+      });
     });
   } else if (search[0] === 'author') {
     const url = `https://www.googleapis.com/books/v1/volumes?q=+inauthor:${search[1]}`;
     superagent.get(url).then(obj => {
 
       const books = obj.body.items.map(item => new Book(item));
-
-      // console.log(books);
-      res.render('pages/searches/show.ejs', { books: books });
+      const sqlSend = 'INSERT INTO books (title, author, url';
+      books.forEach(obj => {
+        const sqlArr = [books[obj].title, books[obj].author, books[obj].url];
+        client.query(sqlSend, sqlArr);
+      });
 
 
 
